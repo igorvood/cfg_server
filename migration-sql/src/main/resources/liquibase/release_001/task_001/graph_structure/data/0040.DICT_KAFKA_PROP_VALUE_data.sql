6caa272533d78e4479055f973a39c7e2b3246266
vood <balitskY_igor@list.ru>
2022-12-16 17:41:37 +0300

    import/export rest

--- migration-sql/src/main/resources/liquibase/release_001/task_001/graph_structure/data/0040.DICT_KAFKA_PROP_VALUE_data.sql
+++ migration-sql/src/main/resources/liquibase/release_001/task_001/graph_structure/data/0040.DICT_KAFKA_PROP_VALUE_data.sql
@@ -2,13 +2,13 @@ DO $$
 begin
 
 
-INSERT INTO DICT_KAFKA_PROP_VALUE (GRP_ID, TYPE_PROP, PROP_ID, PROP_VALUE) VALUES ('consumer_default', 'cns', 'ssl.truststore.password', '${DSO_KAFKA_SSL_TRUSTSTORE_PASSWORD}');
-INSERT INTO DICT_KAFKA_PROP_VALUE (GRP_ID, TYPE_PROP, PROP_ID, PROP_VALUE) VALUES ('consumer_default', 'cns', 'ssl.truststore.location', '${TRUSTSTORE_LOCATION}');
-INSERT INTO DICT_KAFKA_PROP_VALUE (GRP_ID, TYPE_PROP, PROP_ID, PROP_VALUE) VALUES ('consumer_default', 'cns', 'ssl.keystore.password', '${DSO_KAFKA_SSL_KEYSTORE_PASSWORD}');
-INSERT INTO DICT_KAFKA_PROP_VALUE (GRP_ID, TYPE_PROP, PROP_ID, PROP_VALUE) VALUES ('consumer_default', 'cns', 'ssl.keystore.location', '${KEYSTORE_LOCATION} ');
-INSERT INTO DICT_KAFKA_PROP_VALUE (GRP_ID, TYPE_PROP, PROP_ID, PROP_VALUE) VALUES ('consumer_default', 'cns', 'ssl.key.password', '${DSO_KAFKA_SSL_KEY_PASSWORD}');
-INSERT INTO DICT_KAFKA_PROP_VALUE (GRP_ID, TYPE_PROP, PROP_ID, PROP_VALUE) VALUES ('consumer_default', 'cns', 'security.protocol', 'SSL');
-INSERT INTO DICT_KAFKA_PROP_VALUE (GRP_ID, TYPE_PROP, PROP_ID, PROP_VALUE) VALUES ('consumer_default', 'cns', 'bootstrap.servers', '${BOOTSTRAP_SERVERS}');
+INSERT INTO DICT_KAFKA_PROP_VALUE (GRP_ID, TYPE_PROP, PROP_ID, PROP_VALUE) VALUES ('earliest', 'cns', 'ssl.truststore.password', '${DSO_KAFKA_SSL_TRUSTSTORE_PASSWORD}');
+INSERT INTO DICT_KAFKA_PROP_VALUE (GRP_ID, TYPE_PROP, PROP_ID, PROP_VALUE) VALUES ('earliest', 'cns', 'ssl.truststore.location', '${TRUSTSTORE_LOCATION}');
+INSERT INTO DICT_KAFKA_PROP_VALUE (GRP_ID, TYPE_PROP, PROP_ID, PROP_VALUE) VALUES ('earliest', 'cns', 'ssl.keystore.password', '${DSO_KAFKA_SSL_KEYSTORE_PASSWORD}');
+INSERT INTO DICT_KAFKA_PROP_VALUE (GRP_ID, TYPE_PROP, PROP_ID, PROP_VALUE) VALUES ('earliest', 'cns', 'ssl.keystore.location', '${KEYSTORE_LOCATION} ');
+INSERT INTO DICT_KAFKA_PROP_VALUE (GRP_ID, TYPE_PROP, PROP_ID, PROP_VALUE) VALUES ('earliest', 'cns', 'ssl.key.password', '${DSO_KAFKA_SSL_KEY_PASSWORD}');
+INSERT INTO DICT_KAFKA_PROP_VALUE (GRP_ID, TYPE_PROP, PROP_ID, PROP_VALUE) VALUES ('earliest', 'cns', 'security.protocol', 'SSL');
+INSERT INTO DICT_KAFKA_PROP_VALUE (GRP_ID, TYPE_PROP, PROP_ID, PROP_VALUE) VALUES ('earliest', 'cns', 'bootstrap.servers', '${BOOTSTRAP_SERVERS}');
 
 INSERT INTO DICT_KAFKA_PROP_VALUE (GRP_ID, TYPE_PROP, PROP_ID, PROP_VALUE) VALUES ('producer_default', 'prd', 'bootstrap.servers', '${BOOTSTRAP_SERVERS}');
 INSERT INTO DICT_KAFKA_PROP_VALUE (GRP_ID, TYPE_PROP, PROP_ID, PROP_VALUE) VALUES ('producer_default', 'prd', 'security.protocol', 'SSL');
@@ -18,22 +18,22 @@ INSERT INTO DICT_KAFKA_PROP_VALUE (GRP_ID, TYPE_PROP, PROP_ID, PROP_VALUE) VALUE
 INSERT INTO DICT_KAFKA_PROP_VALUE (GRP_ID, TYPE_PROP, PROP_ID, PROP_VALUE) VALUES ('producer_default', 'prd', 'ssl.truststore.location', '${TRUSTSTORE_LOCATION}');
 INSERT INTO DICT_KAFKA_PROP_VALUE (GRP_ID, TYPE_PROP, PROP_ID, PROP_VALUE) VALUES ('producer_default', 'prd', 'ssl.truststore.password', '${DSO_KAFKA_SSL_TRUSTSTORE_PASSWORD}');
 
-INSERT INTO DICT_KAFKA_PROP_VALUE (GRP_ID, TYPE_PROP, PROP_ID, PROP_VALUE) VALUES ('consumer_default', 'cns', 'auto.offset.reset', 'earliest');
--- INSERT INTO DICT_KAFKA_PROP_VALUE (GRP_ID, TYPE_PROP, PROP_ID, PROP_VALUE) VALUES ('consumer_default', 'cns', 'check.crcs', 'true');
--- INSERT INTO DICT_KAFKA_PROP_VALUE (GRP_ID, TYPE_PROP, PROP_ID, PROP_VALUE) VALUES ('consumer_default', 'cns', 'connections.max.idle.ms', '540000');
--- INSERT INTO DICT_KAFKA_PROP_VALUE (GRP_ID, TYPE_PROP, PROP_ID, PROP_VALUE) VALUES ('consumer_default', 'cns', 'enable.auto.commit', 'true');
--- INSERT INTO DICT_KAFKA_PROP_VALUE (GRP_ID, TYPE_PROP, PROP_ID, PROP_VALUE) VALUES ('consumer_default', 'cns', 'exclude.internal.topics', 'true');
--- INSERT INTO DICT_KAFKA_PROP_VALUE (GRP_ID, TYPE_PROP, PROP_ID, PROP_VALUE) VALUES ('consumer_default', 'cns', 'fetch.max.bytes', '52428800');
--- INSERT INTO DICT_KAFKA_PROP_VALUE (GRP_ID, TYPE_PROP, PROP_ID, PROP_VALUE) VALUES ('consumer_default', 'cns', 'fetch.max.wait.ms', '500');
-INSERT INTO DICT_KAFKA_PROP_VALUE (GRP_ID, TYPE_PROP, PROP_ID, PROP_VALUE) VALUES ('consumer_default', 'cns', 'fetch.min.bytes', '50');
+INSERT INTO DICT_KAFKA_PROP_VALUE (GRP_ID, TYPE_PROP, PROP_ID, PROP_VALUE) VALUES ('earliest', 'cns', 'auto.offset.reset', 'earliest');
+-- INSERT INTO DICT_KAFKA_PROP_VALUE (GRP_ID, TYPE_PROP, PROP_ID, PROP_VALUE) VALUES ('earliest', 'cns', 'check.crcs', 'true');
+-- INSERT INTO DICT_KAFKA_PROP_VALUE (GRP_ID, TYPE_PROP, PROP_ID, PROP_VALUE) VALUES ('earliest', 'cns', 'connections.max.idle.ms', '540000');
+-- INSERT INTO DICT_KAFKA_PROP_VALUE (GRP_ID, TYPE_PROP, PROP_ID, PROP_VALUE) VALUES ('earliest', 'cns', 'enable.auto.commit', 'true');
+-- INSERT INTO DICT_KAFKA_PROP_VALUE (GRP_ID, TYPE_PROP, PROP_ID, PROP_VALUE) VALUES ('earliest', 'cns', 'exclude.internal.topics', 'true');
+-- INSERT INTO DICT_KAFKA_PROP_VALUE (GRP_ID, TYPE_PROP, PROP_ID, PROP_VALUE) VALUES ('earliest', 'cns', 'fetch.max.bytes', '52428800');
+-- INSERT INTO DICT_KAFKA_PROP_VALUE (GRP_ID, TYPE_PROP, PROP_ID, PROP_VALUE) VALUES ('earliest', 'cns', 'fetch.max.wait.ms', '500');
+INSERT INTO DICT_KAFKA_PROP_VALUE (GRP_ID, TYPE_PROP, PROP_ID, PROP_VALUE) VALUES ('earliest', 'cns', 'fetch.min.bytes', '50');
 
-INSERT INTO DICT_KAFKA_PROP_VALUE (GRP_ID, TYPE_PROP, PROP_ID, PROP_VALUE) VALUES ('consumer_default', 'cns', 'isolation.level', 'read_uncommitted');
-INSERT INTO DICT_KAFKA_PROP_VALUE (GRP_ID, TYPE_PROP, PROP_ID, PROP_VALUE) VALUES ('consumer_default', 'cns', 'ssl.truststore.type', 'PKCS12');
-INSERT INTO DICT_KAFKA_PROP_VALUE (GRP_ID, TYPE_PROP, PROP_ID, PROP_VALUE) VALUES ('consumer_default', 'cns', 'ssl.keystore.type', 'PKCS12');
-INSERT INTO DICT_KAFKA_PROP_VALUE (GRP_ID, TYPE_PROP, PROP_ID, PROP_VALUE) VALUES ('consumer_default', 'cns', 'session.timeout.ms', '72000000');
+INSERT INTO DICT_KAFKA_PROP_VALUE (GRP_ID, TYPE_PROP, PROP_ID, PROP_VALUE) VALUES ('earliest', 'cns', 'isolation.level', 'read_uncommitted');
+INSERT INTO DICT_KAFKA_PROP_VALUE (GRP_ID, TYPE_PROP, PROP_ID, PROP_VALUE) VALUES ('earliest', 'cns', 'ssl.truststore.type', 'PKCS12');
+INSERT INTO DICT_KAFKA_PROP_VALUE (GRP_ID, TYPE_PROP, PROP_ID, PROP_VALUE) VALUES ('earliest', 'cns', 'ssl.keystore.type', 'PKCS12');
+INSERT INTO DICT_KAFKA_PROP_VALUE (GRP_ID, TYPE_PROP, PROP_ID, PROP_VALUE) VALUES ('earliest', 'cns', 'session.timeout.ms', '72000000');
 
--- INSERT INTO DICT_KAFKA_PROP_VALUE (GRP_ID, TYPE_PROP, PROP_ID, PROP_VALUE) VALUES ('consumer_default', 'cns', 'group.id', '${PROFILE}');
--- INSERT INTO DICT_KAFKA_PROP_VALUE (GRP_ID, TYPE_PROP, PROP_ID, PROP_VALUE) VALUES ('consumer_default', 'cns', 'heartbeat.interval.ms', '3000');
+-- INSERT INTO DICT_KAFKA_PROP_VALUE (GRP_ID, TYPE_PROP, PROP_ID, PROP_VALUE) VALUES ('earliest', 'cns', 'group.id', '${PROFILE}');
+-- INSERT INTO DICT_KAFKA_PROP_VALUE (GRP_ID, TYPE_PROP, PROP_ID, PROP_VALUE) VALUES ('earliest', 'cns', 'heartbeat.interval.ms', '3000');
 
 -- INSERT INTO DICT_KAFKA_PROP_VALUE (GRP_ID, TYPE_PROP, PROP_ID, PROP_VALUE) VALUES ('producer_default', 'prd', 'acks', 'all');
 -- INSERT INTO DICT_KAFKA_PROP_VALUE (GRP_ID, TYPE_PROP, PROP_ID, PROP_VALUE) VALUES ('producer_default', 'prd', 'enable.idempotence', 'true');
@@ -46,7 +46,7 @@ INSERT INTO DICT_KAFKA_PROP_VALUE (GRP_ID, TYPE_PROP, PROP_ID, PROP_VALUE) VALUE
 
 
 
-INSERT INTO dict_kafka_prop_value (grp_id, type_prop, prop_id, prop_value) VALUES ('latest', 'cns', 'auto.offset.reset', 'earliest');
+INSERT INTO dict_kafka_prop_value (grp_id, type_prop, prop_id, prop_value) VALUES ('latest', 'cns', 'auto.offset.reset', 'latest');
 INSERT INTO dict_kafka_prop_value (grp_id, type_prop, prop_id, prop_value) VALUES ('latest', 'cns', 'bootstrap.servers', '${BOOTSTRAP_SERVERS}');
 INSERT INTO dict_kafka_prop_value (grp_id, type_prop, prop_id, prop_value) VALUES ('latest', 'cns', 'fetch.min.bytes', '50');
 INSERT INTO dict_kafka_prop_value (grp_id, type_prop, prop_id, prop_value) VALUES ('latest', 'cns', 'isolation.level', 'read_uncommitted');
--- migration-sql/src/main/resources/liquibase/release_001/task_001/graph_structure/data/0220.dict_arrow.sql
+++ migration-sql/src/main/resources/liquibase/release_001/task_001/graph_structure/data/0220.dict_arrow.sql
@@ -24,10 +24,10 @@ INSERT INTO DICT_ARROW (GRAPH_ID, BEG_NODE_TYPE, BEG_NODE_ID, END_NODE_TYPE, END
 -- select 'graph_1', 'topic', 'uasp_mutator_filter_input', 'flink_srv', 'Mutator~case_65', 'some_key9' from DUAL union
 -- select 'graph_1', 'topic', 'uasp_mutator_filter_input_2', 'flink_srv', 'Mutator~case_65', 'some_key30' from DUAL union
 
-select 'rto_graph', 'topic', 'dev_bevents_68_after_rate_enrich', 'flink_srv', 'uasp-kriaa-bevents-case-2~main', 'enrichOne.MainEnrichProperty$.fromTopic', 'consumer_default' from DUAL union
-select 'rto_graph', 'topic', 'dev_bevents_68_after_rate_enrich', 'flink_srv', 'uasp-kriaa-bevents-case-40~main', 'enrichOne.MainEnrichProperty$.fromTopic', 'consumer_default' from DUAL union
-select 'rto_graph', 'topic', 'dev_multibonus_partners_program', 'flink_srv', 'uasp-kriaa-bevents-case-40~main', 'multiBonus.MultiBonusEnrichProperty$.fromTopic', 'consumer_default' from DUAL union
-select 'rto_graph', 'topic', 'dev_bevents_68_after_rate_enrich', 'flink_srv', 'uasp-kriaa-bevents-case-68~main', 'enrichOne.MainEnrichProperty$.fromTopic', 'consumer_default' from DUAL union
+select 'rto_graph', 'topic', 'dev_bevents_68_after_rate_enrich', 'flink_srv', 'uasp-kriaa-bevents-case-2~main', 'enrichOne.MainEnrichProperty$.fromTopic', 'earliest' from DUAL union
+select 'rto_graph', 'topic', 'dev_bevents_68_after_rate_enrich', 'flink_srv', 'uasp-kriaa-bevents-case-40~main', 'enrichOne.MainEnrichProperty$.fromTopic', 'earliest' from DUAL union
+select 'rto_graph', 'topic', 'dev_multibonus_partners_program', 'flink_srv', 'uasp-kriaa-bevents-case-40~main', 'multiBonus.MultiBonusEnrichProperty$.fromTopic', 'earliest' from DUAL union
+select 'rto_graph', 'topic', 'dev_bevents_68_after_rate_enrich', 'flink_srv', 'uasp-kriaa-bevents-case-68~main', 'enrichOne.MainEnrichProperty$.fromTopic', 'earliest' from DUAL union
 select 'rto_graph', 'flink_srv', 'uasp-kriaa-bevents-case-40~main', 'topic', 'dev__rto_uasp__case_40__dlq', 'enrichOne.MainEnrichProperty$.dlq.FlinkSinkProperties$.toTopic', 'producer_default' from DUAL union
 select 'rto_graph', 'flink_srv', 'uasp-kriaa-bevents-case-40~main', 'topic', 'dev__rto_uasp__case_40', 'enrichOne.MainEnrichProperty$.out.FlinkSinkProperties$.toTopic', 'producer_default' from DUAL union
 select 'rto_graph', 'flink_srv', 'uasp-kriaa-bevents-case-68~main', 'topic', 'dev__rto_uasp__pks', 'enrichOne.MainEnrichProperty$.out.FlinkSinkProperties$.toTopic', 'producer_default' from DUAL union
@@ -44,37 +44,37 @@ select 'rto_graph', 'flink_srv', 'uasp-streaming-mdm-enrichment~way4-card-agreem
 select 'rto_graph', 'flink_srv', 'uasp-streaming-mdm-enrichment~way4-card-agreement', 'topic', 'dev_bevents_card_agreement_enrich_way4_dlq', 'enrichOne.MainEnrichProperty$.dlq.FlinkSinkProperties$.toTopic', 'producer_default' from DUAL union
 select 'rto_graph', 'flink_srv', 'uasp-streaming-mdm-enrichment~way4-card-agreement', 'topic', 'dev_bevents_card_agreement_enrich_out_uaspdto', 'enrichOne.MainEnrichProperty$.out.FlinkSinkProperties$.toTopic', 'producer_default' from DUAL union
 
-select 'rto_graph', 'topic', 'dev_ivr__uasp_realtime__input_converter__mortgage__uaspdto', 'flink_srv', 'uasp-streaming-mdm-enrichment~way4', 'enrichOne.CommonEnrichProperty$.fromTopic', 'consumer_default' from DUAL union
-select 'rto_graph', 'topic', 'dev_ivr__uasp_realtime__input_converter__mdm_cross_link__uaspdto', 'flink_srv', 'uasp-streaming-mdm-enrichment~way4', 'enrichOne.GlobalIdEnrichProperty$.fromTopic', 'consumer_default' from DUAL union
+select 'rto_graph', 'topic', 'dev_ivr__uasp_realtime__input_converter__mortgage__uaspdto', 'flink_srv', 'uasp-streaming-mdm-enrichment~way4', 'enrichOne.CommonEnrichProperty$.fromTopic', 'earliest' from DUAL union
+select 'rto_graph', 'topic', 'dev_ivr__uasp_realtime__input_converter__mdm_cross_link__uaspdto', 'flink_srv', 'uasp-streaming-mdm-enrichment~way4', 'enrichOne.GlobalIdEnrichProperty$.fromTopic', 'earliest' from DUAL union
 select 'rto_graph', 'topic', 'dev_ivr__uasp_realtime__input_converter__way4_issuing_operation__uaspdto', 'flink_srv', 'uasp-streaming-mdm-enrichment~way4', 'enrichOne.MainEnrichProperty$.fromTopic', 'latest' from DUAL union
 
-select 'rto_graph', 'topic', 'dev__dko_uasp__card_agreement_converted', 'flink_srv', 'uasp-streaming-mdm-enrichment~way4-card-agreement', 'enrichOne.CommonEnrichProperty$.fromTopic', 'consumer_default' from DUAL union
+select 'rto_graph', 'topic', 'dev__dko_uasp__card_agreement_converted', 'flink_srv', 'uasp-streaming-mdm-enrichment~way4-card-agreement', 'enrichOne.CommonEnrichProperty$.fromTopic', 'earliest' from DUAL union
 select 'rto_graph', 'topic', 'dev_ivr__uasp_realtime__case_48_concatenate__uaspdto', 'flink_srv', 'uasp-streaming-mdm-enrichment~way4-card-agreement', 'enrichOne.MainEnrichProperty$.fromTopic', 'latest' from DUAL union
 
 
 select 'rto_graph', 'topic', 'dev_bevents_cft_way4_profile_udds_before_mdm_rate_case68_uaspdto', 'flink_srv', 'uasp-streaming-mdm-enrichment~case-68', 'enrichOne.MainEnrichProperty$.fromTopic', 'latest' from DUAL union
-select 'rto_graph', 'topic', 'dev_input_converter_cardfl_refill_uasp', 'flink_srv', 'uasp-streaming-mdm-enrichment~case-68', 'enrichOne.CommonEnrichProperty$.fromTopic', 'consumer_default' from DUAL union
+select 'rto_graph', 'topic', 'dev_input_converter_cardfl_refill_uasp', 'flink_srv', 'uasp-streaming-mdm-enrichment~case-68', 'enrichOne.CommonEnrichProperty$.fromTopic', 'earliest' from DUAL union
 select 'rto_graph', 'flink_srv', 'uasp-streaming-mdm-enrichment~case-68', 'topic', 'dev_bevents_cft_way4_profile_udds_before_case68_uaspdto', 'enrichOne.MainEnrichProperty$.out.FlinkSinkProperties$.toTopic', 'producer_default' from DUAL union
 select 'rto_graph', 'flink_srv', 'uasp-streaming-mdm-enrichment~case-68', 'topic', 'dev_bevents_udds_mdm_rate_case68_uaspdto_dlq', 'enrichOne.MainEnrichProperty$.dlq.FlinkSinkProperties$.toTopic', 'producer_default' from DUAL union
 select 'rto_graph', 'flink_srv', 'uasp-streaming-mdm-enrichment~case-68', 'topic', 'dev_input_converter_cardfl_refill_uasp_dlq', 'enrichOne.CommonEnrichProperty$.dlq.FlinkSinkProperties$.toTopic', 'producer_default' from DUAL union
 
 select 'rto_graph', 'topic', 'dev_bevents_cft_way4_profile_udds_before_case68_uaspdto', 'flink_srv', 'uasp-streaming-mdm-enrichment~case-68_agrement', 'enrichOne.MainEnrichProperty$.fromTopic', 'latest' from DUAL union
-select 'rto_graph', 'topic', 'dev__dko_uasp__card_agreement_converted', 'flink_srv', 'uasp-streaming-mdm-enrichment~case-68_agrement', 'enrichOne.CommonEnrichProperty$.fromTopic', 'consumer_default' from DUAL union
+select 'rto_graph', 'topic', 'dev__dko_uasp__card_agreement_converted', 'flink_srv', 'uasp-streaming-mdm-enrichment~case-68_agrement', 'enrichOne.CommonEnrichProperty$.fromTopic', 'earliest' from DUAL union
 select 'rto_graph', 'flink_srv', 'uasp-streaming-mdm-enrichment~case-68_agrement', 'topic', 'dev_bevents_cft_way4_profile_udds_before_case68_uaspdto_dlq', 'enrichOne.MainEnrichProperty$.dlq.FlinkSinkProperties$.toTopic', 'producer_default' from DUAL union
 select 'rto_graph', 'flink_srv', 'uasp-streaming-mdm-enrichment~case-68_agrement', 'topic', 'dev_bevents_udds_mdm_rate_case68_uaspdto', 'enrichOne.MainEnrichProperty$.out.FlinkSinkProperties$.toTopic', 'producer_default' from DUAL union
 select 'rto_graph', 'flink_srv', 'uasp-streaming-mdm-enrichment~case-68_agrement', 'topic', 'dev_rto_batch_ca_customer_card68_uaspdto_dlq', 'enrichOne.CommonEnrichProperty$.dlq.FlinkSinkProperties$.toTopic', 'producer_default' from DUAL union
 
 select 'rto_graph', 'topic', 'dev_bevents_streaming_input_convertor_profile_auth_uaspdto', 'flink_srv', 'uasp-streaming-mdm-enrichment~prof-auth', 'enrichOne.MainEnrichProperty$.fromTopic', 'latest' from DUAL union
-select 'rto_graph', 'topic', 'dev__dko_uasp__card_agreement_converted', 'flink_srv', 'uasp-streaming-mdm-enrichment~prof-auth', 'enrichOne.GlobalIdEnrichProperty$.fromTopic', 'consumer_default' from DUAL union
+select 'rto_graph', 'topic', 'dev__dko_uasp__card_agreement_converted', 'flink_srv', 'uasp-streaming-mdm-enrichment~prof-auth', 'enrichOne.GlobalIdEnrichProperty$.fromTopic', 'earliest' from DUAL union
 select 'rto_graph', 'flink_srv', 'uasp-streaming-mdm-enrichment~prof-auth', 'topic', 'dev_bevents_card_agreement_enrich_dlq', 'enrichOne.MainEnrichProperty$.dlq.FlinkSinkProperties$.toTopic', 'producer_default' from DUAL union
 select 'rto_graph', 'flink_srv', 'uasp-streaming-mdm-enrichment~prof-auth', 'topic', 'dev_bevents_card_agreement_enrich_out_uaspdto', 'enrichOne.MainEnrichProperty$.out.FlinkSinkProperties$.toTopic', 'producer_default' from DUAL union
 select 'rto_graph', 'flink_srv', 'uasp-streaming-mdm-enrichment~prof-auth', 'topic', 'dev_rto_batch_ca_customer_card_uaspdto__dlq', 'enrichOne.GlobalIdEnrichProperty$.dlq.FlinkSinkProperties$.toTopic', 'producer_default' from DUAL union
 
-select 'rto_graph', 'topic', 'dev_ivr__uasp_realtime__ca_ha_aggregate__ca__uaspdto', 'flink_srv', 'uasp-streaming-aggregate~aggregate-dko', 'consumer.ca.topic.name', 'consumer_default'  from DUAL union
+select 'rto_graph', 'topic', 'dev_ivr__uasp_realtime__ca_ha_aggregate__ca__uaspdto', 'flink_srv', 'uasp-streaming-aggregate~aggregate-dko', 'consumer.ca.topic.name', 'earliest'  from DUAL union
 select 'rto_graph', 'flink_srv', 'uasp-streaming-aggregate~aggregate-dko', 'topic', 'dev_ivr__uasp_realtime__aggregate__uaspdto', 'producer.ha.topic.name', 'producer_default'  from DUAL union
 select 'rto_graph', 'flink_srv', 'uasp-streaming-input-convertor~input-convertor-w4-operation', 'topic', 'dev_ivr__uasp_realtime__input_converter__way4_issuing_operation__uaspdto', 'way4.output.topic.name', 'producer_default'  from DUAL union
-select 'rto_graph', 'topic', 'dev_ivr__uasp_realtime__mdm_enrichment__uaspdto', 'flink_srv', 'uasp-streaming-aggregate~aggregate-dko', 'consumer.ha.topic.name', 'consumer_default'  from DUAL union
-select 'rto_graph', 'topic', 'dev_ivr__uasp_realtime__input_converter__way4_issuing_operation__json', 'flink_srv', 'uasp-streaming-input-convertor~input-convertor-w4-operation', 'way4.input.topic.name', 'consumer_default'  from DUAL union
+select 'rto_graph', 'topic', 'dev_ivr__uasp_realtime__mdm_enrichment__uaspdto', 'flink_srv', 'uasp-streaming-aggregate~aggregate-dko', 'consumer.ha.topic.name', 'earliest'  from DUAL union
+select 'rto_graph', 'topic', 'dev_ivr__uasp_realtime__input_converter__way4_issuing_operation__json', 'flink_srv', 'uasp-streaming-input-convertor~input-convertor-w4-operation', 'way4.input.topic.name', 'earliest'  from DUAL union
 select 'rto_graph', 'flink_srv', 'uasp-streaming-aggregate~aggregate-dko', 'topic', 'dev_ivr__uasp_realtime__aggregate__filter', 'producer.ha.filter.status.topic.name', 'producer_default'  from DUAL union
 select 'rto_graph', 'flink_srv', 'uasp-streaming-input-convertor~input-convertor-w4-operation', 'topic', 'dev_ivr__uasp_realtime__input_converter__way4_issuing_operation__dlq', 'way4.dlq.topic.name', 'producer_default'  from DUAL union
 select 'rto_graph', 'flink_srv', 'uasp-streaming-aggregate~aggregate-dko', 'topic', 'dev_ivr__uasp_realtime__aggregate__dlq', 'producer.ha.dlq.topic.name', 'producer_default'  from DUAL union
@@ -82,24 +82,24 @@ select 'rto_graph', 'flink_srv', 'uasp-streaming-aggregate~aggregate-dko', 'topi
 
 
 select 'rto_graph', 'topic', 'dev_bevents_card_agreement_enrich_out_uaspdto', 'flink_srv', 'uasp-streaming-mdm-enrichment~prof-auth-packNM', 'enrichOne.MainEnrichProperty$.fromTopic', 'latest' from DUAL union
-select 'rto_graph', 'topic', 'dev__dko_uasp__pension_converted', 'flink_srv', 'uasp-streaming-mdm-enrichment~prof-auth-packNM', 'enrichOne.CommonEnrichProperty$.fromTopic', 'consumer_default' from DUAL union
+select 'rto_graph', 'topic', 'dev__dko_uasp__pension_converted', 'flink_srv', 'uasp-streaming-mdm-enrichment~prof-auth-packNM', 'enrichOne.CommonEnrichProperty$.fromTopic', 'earliest' from DUAL union
 select 'rto_graph', 'flink_srv', 'uasp-streaming-mdm-enrichment~prof-auth-packNM', 'topic', 'dev_bevents_package_nm_enrich_dlq', 'enrichOne.MainEnrichProperty$.dlq.FlinkSinkProperties$.toTopic', 'producer_default' from DUAL union
 select 'rto_graph', 'flink_srv', 'uasp-streaming-mdm-enrichment~prof-auth-packNM', 'topic', 'dev_bevents__realtime__enrichment__prof__transactions_first__uaspdto', 'enrichOne.MainEnrichProperty$.out.FlinkSinkProperties$.toTopic', 'producer_default' from DUAL union
 select 'rto_graph', 'flink_srv', 'uasp-streaming-mdm-enrichment~prof-auth-packNM', 'topic', 'dev_rto_batch_ca_customer_package_dlq', 'enrichOne.CommonEnrichProperty$.dlq.FlinkSinkProperties$.toTopic', 'producer_default' from DUAL union
 
 select 'rto_graph', 'topic', 'dev_bevents__realtime__input_converter__prof__transactions__uaspdto', 'flink_srv', 'uasp-streaming-mdm-enrichment~prof-tx-case-71', 'enrichOne.MainEnrichProperty$.fromTopic', 'latest' from DUAL union
-select 'rto_graph', 'topic', 'dev_rto_batch_ca_deposit_account_case_71_json_converted', 'flink_srv', 'uasp-streaming-mdm-enrichment~prof-tx-case-71', 'enrichOne.CommonEnrichProperty$.fromTopic', 'consumer_default' from DUAL union
+select 'rto_graph', 'topic', 'dev_rto_batch_ca_deposit_account_case_71_json_converted', 'flink_srv', 'uasp-streaming-mdm-enrichment~prof-tx-case-71', 'enrichOne.CommonEnrichProperty$.fromTopic', 'earliest' from DUAL union
 select 'rto_graph', 'flink_srv', 'uasp-streaming-mdm-enrichment~prof-tx-case-71', 'topic', 'dev_bevents__realtime__case_71__uaspdto', 'enrichOne.MainEnrichProperty$.out.FlinkSinkProperties$.toTopic', 'producer_default' from DUAL union
 select 'rto_graph', 'flink_srv', 'uasp-streaming-mdm-enrichment~prof-tx-case-71', 'topic', 'dev_bevents__realtime__case_71__uaspdto_dlq', 'enrichOne.MainEnrichProperty$.dlq.FlinkSinkProperties$.toTopic', 'producer_default' from DUAL union
 select 'rto_graph', 'flink_srv', 'uasp-streaming-mdm-enrichment~prof-tx-case-71', 'topic', 'dev_rto_batch_ca_deposit_account_case_71_uaspdto__dlq', 'enrichOne.CommonEnrichProperty$.dlq.FlinkSinkProperties$.toTopic', 'producer_default' from DUAL union
 
-select 'rto_graph', 'topic', 'dev_bevents__batch__ca_regulatory_client_id_of_profile__json_converted', 'flink_srv', 'uasp-streaming-mdm-enrichment~profile-tx-step1', 'enrichOne.GlobalIdEnrichProperty$.fromTopic', 'consumer_default' from DUAL union
+select 'rto_graph', 'topic', 'dev_bevents__batch__ca_regulatory_client_id_of_profile__json_converted', 'flink_srv', 'uasp-streaming-mdm-enrichment~profile-tx-step1', 'enrichOne.GlobalIdEnrichProperty$.fromTopic', 'earliest' from DUAL union
 select 'rto_graph', 'topic', 'dev_bevents__realtime__case_71__uaspdto', 'flink_srv', 'uasp-streaming-mdm-enrichment~profile-tx-step1', 'enrichOne.MainEnrichProperty$.fromTopic', 'latest' from DUAL union
 select 'rto_graph', 'flink_srv', 'uasp-streaming-mdm-enrichment~profile-tx-step1', 'topic', 'dev_bevents__realtime__enrichment__prof__contract_num__uaspdto', 'enrichOne.MainEnrichProperty$.out.FlinkSinkProperties$.toTopic', 'producer_default' from DUAL union
 select 'rto_graph', 'flink_srv', 'uasp-streaming-mdm-enrichment~profile-tx-step1', 'topic', 'dev_bevents__realtime__enrichment__prof__transactions_with_clientid__dlq', 'enrichOne.GlobalIdEnrichProperty$.dlq.FlinkSinkProperties$.toTopic', 'producer_default' from DUAL union
 select 'rto_graph', 'flink_srv', 'uasp-streaming-mdm-enrichment~profile-tx-step1', 'topic', 'dev_bevents__realtime__enrichment__prof__transactions_with_contract_num__dlq', 'enrichOne.MainEnrichProperty$.dlq.FlinkSinkProperties$.toTopic', 'producer_default' from DUAL union
 
-select 'rto_graph', 'topic', 'dev_bevents__batch__ca_regulatory_contract_num_of_mdm_id_profile__uaspdto', 'flink_srv', 'uasp-streaming-mdm-enrichment~profile-tx-step2', 'enrichOne.GlobalIdEnrichProperty$.fromTopic', 'consumer_default' from DUAL union
+select 'rto_graph', 'topic', 'dev_bevents__batch__ca_regulatory_contract_num_of_mdm_id_profile__uaspdto', 'flink_srv', 'uasp-streaming-mdm-enrichment~profile-tx-step2', 'enrichOne.GlobalIdEnrichProperty$.fromTopic', 'earliest' from DUAL union
 select 'rto_graph', 'topic', 'dev_bevents__realtime__enrichment__prof__contract_num__uaspdto', 'flink_srv', 'uasp-streaming-mdm-enrichment~profile-tx-step2', 'enrichOne.MainEnrichProperty$.fromTopic', 'latest' from DUAL union
 select 'rto_graph', 'flink_srv', 'uasp-streaming-mdm-enrichment~profile-tx-step2', 'topic', 'dev_bevents__realtime__enrichment__prof__transactions_first__uaspdto', 'enrichOne.MainEnrichProperty$.out.FlinkSinkProperties$.toTopic', 'producer_default' from DUAL union
 select 'rto_graph', 'flink_srv', 'uasp-streaming-mdm-enrichment~profile-tx-step2', 'topic', 'dev_bevents__realtime__enrichment__prof__transactions_with_mdm_id__dlq', 'enrichOne.GlobalIdEnrichProperty$.dlq.FlinkSinkProperties$.toTopic', 'producer_default' from DUAL union

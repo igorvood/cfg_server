4ecae6177986665d1dfcfef58050bdee1706b22c
vood <balitskY_igor@list.ru>
2022-12-20 15:24:32 +0300

    filter

--- migration-sql/reinst.sh
+++ migration-sql/reinst.sh
@@ -5,3 +5,5 @@ mvn clean package
 java -jar -Dspring.profiles.active=drop ./target/migration-sql-1.0.2.jar
 
 java -jar -Dspring.profiles.active=reinst ./target/migration-sql-1.0.2.jar
+
+java -jar -Dspring.profiles.active=test ./target/migration-sql-1.0.2.jar

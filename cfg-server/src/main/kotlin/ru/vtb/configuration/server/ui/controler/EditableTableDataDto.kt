89914aad6c1694f36290e15a38d14b570c526378
vood <balitskY_igor@list.ru>
2023-01-26 13:41:03 +0300

    filter

--- cfg-server/src/main/kotlin/ru/vtb/configuration/server/ui/controler/EditableTableDataDto.kt
+++ cfg-server/src/main/kotlin/ru/vtb/configuration/server/ui/controler/EditableTableDataDto.kt
@@ -3,4 +3,4 @@ package ru.vtb.configuration.server.ui.controler
 import ru.vtb.configuration.server.backUp.dto.ColumnMeta
 
 data class EditableTableDataDto(val columns: List<ColumnMeta>,
-                                val tableData: List<MutableMap<String, Any>>)
+                                val tableData: List<TableMetaController.RowData>)
--- cfg-server/src/main/kotlin/ru/vtb/configuration/server/ui/controler/TableMetaController.kt
+++ cfg-server/src/main/kotlin/ru/vtb/configuration/server/ui/controler/TableMetaController.kt
@@ -9,15 +9,29 @@ class TableMetaController(private val dataBackUpRepository: IDataBackUpRepositor
 
     fun tablesList(): Set<TableUiDto> {
         val metaDataByTable = dataBackUpRepository.metaDataByTable()
-        return metaDataByTable.map { TableUiDto(it.tableName , it.tableComment) }.toSet()
+        return metaDataByTable.map { TableUiDto(it.tableName, it.tableComment) }.toSet()
     }
 
     fun tableData(tableName: String): EditableTableDataDto {
-        val (lvl, tableId,tableComment ,columns ) = dataBackUpRepository.metaDataByTable(tableName)[0]
+        val (lvl, tableId, tableComment, columns) = dataBackUpRepository.metaDataByTable(tableName)[0]
         val columnList = columns.joinToString(", ") { it.name }
         val tableData = dataBackUpRepository.allTableData("""select $columnList from ${tableName}""")
+            .map { row ->
+                val map = row.map { col -> ColData(col.key, col.value) }
+                RowData(map)
 
-        return EditableTableDataDto(columns, tableData)
+            }
+
+        return EditableTableDataDto(columns.sortedBy { !it.isInPrimaryKey }, tableData)
 
     }
+
+    data class RowData(
+        val cols: List<ColData>
+    )
+
+    data class ColData(
+        val colName: String,
+        val data: Any
+    )
 }

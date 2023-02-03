5e13c95236861e4cd465c06f27ad44e3c313adc2
vood <balitskY_igor@list.ru>
2023-02-03 17:27:41 +0300

    filter

---
+++ cfg-server/src/main/kotlin/ru/vtb/configuration/server/shceduller/ServiceGroupAutomationFiller.kt
@@ -0,0 +1,16 @@
+package ru.vtb.configuration.server.shceduller
+
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Service
+import ru.vtb.configuration.server.repo.DictServiceGroupRepository
+
+
+@Service
+class ServiceGroupAutomationFiller(private val dictServiceGroupRepository: DictServiceGroupRepository) {
+
+    @Scheduled(fixedDelay = 300000)
+    fun scheduleFixedDelayTask() {
+        dictServiceGroupRepository.fillDefaultTracerGroup()
+    }
+
+}

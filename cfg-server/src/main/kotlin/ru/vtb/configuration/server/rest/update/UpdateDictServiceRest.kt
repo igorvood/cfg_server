cb2946472541d18dc500f4b81c00262aac28c465
vood <balitskY_igor@list.ru>
2023-03-13 13:25:50 +0300

    смена профиля тесты

--- cfg-server/src/main/kotlin/ru/vtb/configuration/server/rest/update/UpdateDictServiceRest.kt
+++ cfg-server/src/main/kotlin/ru/vtb/configuration/server/rest/update/UpdateDictServiceRest.kt
@@ -8,26 +8,25 @@ import org.springframework.web.bind.annotation.RestController
 import ru.vtb.configuration.server.controller.move.IMoveDictServiceController
 
 interface IUpdateDictServiceRest {
-    @Operation(summary = "Обновить запись в таблица сервисов", tags = ["Служебные."])
-    @GetMapping("/graphList", produces = [MediaType.APPLICATION_JSON_VALUE])
+
     fun moveDictService(        id: String,
                                 profile: String,
                                 newProfile: String,
-    )
+    ): Int
 }
 
 @RestController
 class UpdateDictServiceRest(private val iMoveDictServiceController: IMoveDictServiceController) :
     IUpdateDictServiceRest {
 
-    @Operation(summary = "переименовать профиль", tags = ["Служебные."])
+    @Operation(summary = "Переименовать профиль. Возвращает кол-во проапдейченных записей.", tags = ["Служебные."])
     @PostMapping("/moveDictService", produces = [MediaType.APPLICATION_JSON_VALUE])
     override fun moveDictService(
         id: String,
         profile: String,
         newProfile: String,
-        ){
-        iMoveDictServiceController.renameProfile(id,profile,newProfile)
+        ): Int{
+        return iMoveDictServiceController.renameProfile(id,profile,newProfile)
 
     }
 }

cf659c7a8b4c7e9036fe1bd2f41be1989719854a
vood <balitskY_igor@list.ru>
2023-03-13 13:10:35 +0300

    смена профиля тесты

---
+++ cfg-server/src/test/kotlin/ru/vtb/configuration/server/controller/move/MoveDictServiceControllerTest.kt
@@ -0,0 +1,40 @@
+package ru.vtb.configuration.server.controller.move
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+import org.springframework.beans.factory.annotation.Autowired
+import ru.vtb.configuration.server.repo.dto.EnvProperty
+import ru.vtb.configuration.server.repo.dto.StandEnum
+import ru.vtb.configuration.server.repo.intf.ConfigurationGeneratorRepository
+import ru.vtb.configuration.server.test.abstraction.AbstractDatasourceTests
+
+internal class MoveDictServiceControllerTest: AbstractDatasourceTests() {
+
+    @Autowired
+    lateinit var iMoveDictServiceController: IMoveDictServiceController
+
+    @Autowired
+    lateinit var configurationGeneratorRepository: ConfigurationGeneratorRepository
+    val serviceId = "uasp-streaming-mdm-enrichment"
+    val profileId = "profile-tx-step1"
+
+
+    @Test
+    fun moveDictService() {
+        val propertyByServiceExpected = configurationGeneratorRepository.propertyByService(serviceId, profileId, StandEnum.P0)
+
+        val newProfile = profileId + "_1"
+        iMoveDictServiceController.renameProfile(serviceId
+            ,profileId,newProfile
+        )
+
+        val propertyByServiceActual = configurationGeneratorRepository.propertyByService(serviceId, newProfile, StandEnum.P0)
+
+        val minus = propertyByServiceExpected.minus(propertyByServiceActual.toSet())
+        Assertions.assertEquals(listOf<EnvProperty>(), minus){ minus.joinToString("\n") { it.toString() } }
+        Assertions.assertEquals(listOf<EnvProperty>(),        propertyByServiceActual.minus(propertyByServiceExpected.toSet()))
+
+
+    }
+}

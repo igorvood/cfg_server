f17032dbefbe4c5f6acbf70429fc2d1c4901d6ec
IBalitskiy <balitsky_igor@list.ru>
2023-03-28 12:41:35 +0300

    смена профиля тесты

---
+++ jpa-processor/src/main/kotlin/ru/vtb/jpaprocessor/annotation/GenerateJpa.kt
@@ -0,0 +1,10 @@
+package ru.vtb.jpaprocessor.annotation
+
+import java.lang.annotation.ElementType
+import java.lang.annotation.Retention
+import java.lang.annotation.RetentionPolicy
+import java.lang.annotation.Target
+
+@Retention(RetentionPolicy.SOURCE)
+@Target(ElementType.TYPE)
+annotation class GenerateJpa()

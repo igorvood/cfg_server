051c90ca55ecc0cef4e6fb5864141cdc4aa40e26
vood <balitsky_igor@list.ru>
2023-04-03 15:12:48 +0300

    Генерация кода

--- jpa-processor/src/main/kotlin/ru/vtb/jpaprocessor/processor/JpaGenerateAnnotationProcessor.kt
+++ jpa-processor/src/main/kotlin/ru/vtb/jpaprocessor/processor/JpaGenerateAnnotationProcessor.kt
@@ -3,6 +3,7 @@ package ru.vtb.jpaprocessor.processor
 import ru.vtb.jpaprocessor.generator.model.AnnotatedEntityClass
 import ru.vtb.jpaprocessor.generator.model.GeneratedJpaRepositoryClass
 import ru.vtb.jpaprocessor.processor.abstraction.AbstractGenerationProcessor
+
 import javax.annotation.processing.*
 import javax.lang.model.SourceVersion
 import javax.lang.model.element.TypeElement

479a8521cd0891ccb572488d286e1dff7a787323
vood <balitsky_igor@list.ru>
2023-04-03 14:48:02 +0300

    удаление не нужно колонки

--- jpa-processor/src/main/kotlin/ru/vtb/jpaprocessor/generator/model/OrIsNullSearchMethod.kt
+++
@@ -1,8 +0,0 @@
-package ru.vtb.jpaprocessor.generator.model
-
-interface OrIsNullSearchMethod {
-    fun name(): String
-    fun entity(): AbstractAnnotatedClass
-    fun filter(): AbstractAnnotatedClass
-    fun query(): String
-}

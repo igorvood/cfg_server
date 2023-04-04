08dbc30c1194573c700cf415a9ea14e89d12e958
vood <balitsky_igor@list.ru>
2023-04-04 19:13:28 +0300

    Генерация кода

--- jpa-annotation-processor-kotlin/src/main/kotlin/ru/vtb/jpaprocessor/kotlin/KotlinImmutableDtoClassBuilder.kt
+++ jpa-annotation-processor-kotlin/src/main/kotlin/ru/vtb/jpaprocessor/kotlin/KotlinImmutableDtoClassBuilder.kt
@@ -2,9 +2,9 @@ package ru.vtb.jpaprocessor.kotlin
 
 import ru.vtb.processor.abstraction.model.GeneratedJpaRepositoryClass
 import ru.vtb.processor.abstraction.model.abstraction.annotation
+import ru.vtb.processor.annotation.GenerateJpa
 import javax.annotation.processing.ProcessingEnvironment
 import javax.persistence.Column
-
 
 
 /**
@@ -20,12 +20,16 @@ class KotlinImmutableDtoClassBuilder(
     roundEnvironment: ProcessingEnvironment
 ) {
 
-    private fun String.mapKotlinType(): String{
-     return   if (this=="java.lang.String") {
-            "String"
-        } else this
+    private fun String.mapKotlinType(): String = if (this == "java.lang.String") {
+        "String"
+    } else this
 
-    }
+    private val generateJpaValue = generatedJpaRepositoryClass.annotatedClass.element.annotation<GenerateJpa>().get()
+
+    val tableComment = generateJpaValue.tableComment
+
+    val genRest = generateJpaValue.genRest
+
 
     private val repositoryClassName = """${className}GeneratedRepository"""
 
@@ -44,7 +48,8 @@ class KotlinImmutableDtoClassBuilder(
         .joinToString(",") { f -> "this.${f.name()}" }
 
 
-    private val mutableToImmutableFun = """fun ${className}.toImmutable() : $immutableClassName =  ${immutableClassName}($listFieldsToImmutable)"""
+    private val mutableToImmutableFun =
+        """fun ${className}.toImmutable() : $immutableClassName =  ${immutableClassName}($listFieldsToImmutable)"""
     private val immutableToMutableFun = """fun $immutableClassName.toMutable() = ${className}().apply { 
 $listFieldsToMutableFun
   }"""
@@ -70,27 +75,31 @@ $mutableToImmutableFun
 
 """
 
-private val restConText=    """
+    private val restConText = if (genRest) """
 @RestController
 class ${className}GeneratedRestApi(
     val $repositoryClassName: $repositoryClassName
 ) {
 
-    @Operation(summary = "$className Найти все", tags = ["Генерированное API"])
+    @Operation(summary = "Найти все.", tags = ["Генерированное API. $tableComment($className)"])
     @GetMapping("/${className}/findAll")
     fun findAll() = $repositoryClassName.findAll().map { it.toImmutable() }
 
-    @Operation(summary = "$className Сохранить", tags = ["Генерированное API"])
+    @Operation(summary = "Сохранить.", tags = ["Генерированное API. $tableComment($className)"])
     @PutMapping("/$className/save")
     fun save(data: $immutableClassName) = $repositoryClassName.save(data.toMutable()).toImmutable()
 
-    @Operation(summary = "$className Удалить по идентификатору", tags = ["Генерированное API"])
+    @Operation(summary = "Удалить по идентификатору.", tags = ["Генерированное API. $tableComment($className)"])
     @DeleteMapping("/$className/deleteById")
-    fun deleteById(id: ${generatedJpaRepositoryClass.annotatedClass.calculateIdClass(roundEnvironment).getOrElse { "Unable to calculate primary key" }.mapKotlinType()}) = $repositoryClassName.deleteById(id)
+    fun deleteById(id: ${
+        generatedJpaRepositoryClass.annotatedClass.calculateIdClass(roundEnvironment)
+            .getOrElse { "Unable to calculate primary key" }.mapKotlinType()
+    }) = $repositoryClassName.deleteById(id)
 
 }
 """
+    else ""
 
-    fun getContent(): String =  contentTemplate+restConText
+    fun getContent(): String = contentTemplate + restConText
 
 }

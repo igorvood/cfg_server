f1e8001f79b8ef0ce118fba9fc9515c4e0097cb8
vood <balitsky_igor@list.ru>
2023-04-14 18:29:54 +0300

    Генерация кода

---
+++ cfg-server/src/test/kotlin/ru/vtb/configuration/server/dataEntity/genRest/dicttopicownerentity/DictTopicOwnerEntityGeneratedRestApiTest.kt
@@ -0,0 +1,30 @@
+package ru.vtb.configuration.server.dataEntity.genRest.dicttopicownerentity
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import org.springframework.beans.factory.annotation.Autowired
+import ru.vtb.configuration.server.abstraction.AbstractDatasourceTests
+import ru.vtb.configuration.server.dataEntity.genRest.dicttopicownerentity.DictTopicOwnerEntityGeneratedRestApi
+
+internal class DictTopicOwnerEntityGeneratedRestApiTest: AbstractDatasourceTests() {
+
+    @Autowired
+    lateinit var dictTopicOwnerEntityGeneratedRestApi: DictTopicOwnerEntityGeneratedRestApi
+
+    @Test
+    fun findAll() {
+    }
+
+    @Test
+    fun save() {
+    }
+
+    @Test
+    fun deleteById() {
+    }
+
+    @Test
+    fun editEntity() {
+    }
+}

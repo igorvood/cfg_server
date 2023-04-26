4aff69d9745df74ae20e2658dc21e0bed8c648ec
vood <balitsky_igor@list.ru>
2023-04-26 17:40:59 +0300

    Генерация кода

--- common-lib/src/main/kotlin/ru/vtb/processor/wrapper/PrimaryKeyWrapper.kt
+++ common-lib/src/main/kotlin/ru/vtb/processor/wrapper/PrimaryKeyWrapper.kt
@@ -1,7 +1,3 @@
 package ru.vtb.processor.wrapper
 
-import com.sun.xml.internal.ws.developer.Serialization
-
-
-@Serialization
 data class PrimaryKeyWrapper<T>(val primaryKey: T)
--- common-lib/src/main/kotlin/ru/vtb/processor/wrapper/RestEditEntityDto.kt
+++ common-lib/src/main/kotlin/ru/vtb/processor/wrapper/RestEditEntityDto.kt
@@ -1,9 +1,7 @@
 package ru.vtb.processor.wrapper
 
-import com.sun.xml.internal.ws.developer.Serialization
 import ru.vtb.processor.intf.IUpdatebleEntity
 
-@Serialization
 data class RestEditEntityDto<PK, DTO>(val primaryKeyWrapper: PrimaryKeyWrapper<PK>,
                                       val newData: DTO,
 )

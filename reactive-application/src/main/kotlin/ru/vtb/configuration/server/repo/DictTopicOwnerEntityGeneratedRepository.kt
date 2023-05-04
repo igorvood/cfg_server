d4def993b1745efc4271e24037316d4550685b20
vood <balitsky_igor@list.ru>
2023-05-04 11:58:57 +0300

    Генерация кода

--- reactive-application/src/main/kotlin/ru/vtb/configuration/server/repo/DictTopicOwnerEntityGeneratedRepository.kt
+++ reactive-application/src/main/kotlin/ru/vtb/configuration/server/repo/DictTopicOwnerEntityGeneratedRepository.kt
@@ -1,7 +1,9 @@
 package ru.vtb.configuration.server.repo
 
+import kotlinx.coroutines.flow.Flow
 import org.springframework.data.r2dbc.repository.Modifying
 import org.springframework.data.r2dbc.repository.R2dbcRepository
+import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 import org.springframework.stereotype.Repository
 import org.springframework.transaction.annotation.Propagation
 import org.springframework.transaction.annotation.Transactional
@@ -10,21 +12,21 @@ import reactor.core.publisher.Mono
 import ru.vtb.configuration.server.dataEntity.DictTopicOwnerEntity
 
 @Repository
-interface DictTopicOwnerEntityGeneratedRepository : R2dbcRepository<DictTopicOwnerEntity, String> {
-    @Modifying
-    @Transactional(propagation = Propagation.MANDATORY)
-    override fun <S : DictTopicOwnerEntity> save(entity: S): Mono<S>
+interface DictTopicOwnerEntityGeneratedRepository : CoroutineCrudRepository<DictTopicOwnerEntity, String> {
 
     @Modifying
     @Transactional(propagation = Propagation.MANDATORY)
-    override fun <S : DictTopicOwnerEntity> saveAll(entities: MutableIterable<S>): Flux<S>
+    override suspend fun deleteAllById(ids: Iterable<String>)
 
     @Modifying
     @Transactional(propagation = Propagation.MANDATORY)
-    override fun deleteById(id: String): Mono<Void>
+    override suspend fun deleteById(id: String)
 
     @Modifying
     @Transactional(propagation = Propagation.MANDATORY)
-    override fun deleteAllById(ids: MutableIterable<String>): Mono<Void>
+    override suspend fun <S : DictTopicOwnerEntity> save(entity: S): DictTopicOwnerEntity
 
+    @Modifying
+    @Transactional(propagation = Propagation.MANDATORY)
+    override fun <S : DictTopicOwnerEntity> saveAll(entities: Iterable<S>): Flow<S>
 }

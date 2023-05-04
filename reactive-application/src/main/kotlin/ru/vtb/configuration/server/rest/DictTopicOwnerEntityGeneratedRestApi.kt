68522a2a1f8e0b43c02f20a5030c95812d0af584
vood <balitsky_igor@list.ru>
2023-05-04 16:31:58 +0300

    Генерация кода

--- reactive-application/src/main/kotlin/ru/vtb/configuration/server/rest/DictTopicOwnerEntityGeneratedRestApi.kt
+++ reactive-application/src/main/kotlin/ru/vtb/configuration/server/rest/DictTopicOwnerEntityGeneratedRestApi.kt
@@ -1,20 +1,25 @@
 package ru.vtb.configuration.server.rest
 
 import io.swagger.v3.oas.annotations.Operation
-import org.springframework.data.r2dbc.repository.R2dbcRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 import org.springframework.http.MediaType
 import org.springframework.transaction.annotation.Propagation
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.bind.annotation.*
 import ru.vtb.configuration.server.dataEntity.DictTopicOwnerEntity
-import ru.vtb.configuration.server.dataEntity.reactive.genRest.dicttopicownerentity.*
-import ru.vtb.processor.wrapper.PrimaryKeyWrapper
-import java.util.*
+import ru.vtb.configuration.server.dataEntity.reactive.genRest.dicttopicownerentity.DictTopicOwnerEntityImmutable
+import ru.vtb.configuration.server.dataEntity.reactive.genRest.dicttopicownerentity.DictTopicOwnerEntityRestEdit
 
+
+import ru.vtb.processor.wrapper.PrimaryKeyWrapper
+
+fun DictTopicOwnerEntity.toImmutable() : DictTopicOwnerEntityImmutable =  DictTopicOwnerEntityImmutable(this.id,this.our,this.descriptionForReport)
 @RestController
-@RequestMapping(path = arrayOf("/DictTopicOwnerEntity"))
+@RequestMapping(path = ["/DictTopicOwnerEntity"])
 class DictTopicOwnerEntityGeneratedRestApi(
-    private val DictTopicOwnerEntityGeneratedRepository: R2dbcRepository<DictTopicOwnerEntity, String>,
+    private val DictTopicOwnerEntityGeneratedRepository: CoroutineCrudRepository<DictTopicOwnerEntity, String>,
 //    private val orIsNullRepository: OrIsNullRepository<DictTopicOwnerEntityFilter, DictTopicOwnerEntity>
 
 )//: IRestHibernateEntity<DictTopicOwnerEntityImmutable, String, DictTopicOwnerEntityFilter>
@@ -25,19 +30,30 @@ class DictTopicOwnerEntityGeneratedRestApi(
         tags = ["Генерированное API. Владелец топика(DictTopicOwnerEntity)"]
     )
     @PutMapping("/findById", produces = [MediaType.APPLICATION_JSON_VALUE])
-    fun findById(@RequestBody id: PrimaryKeyWrapper<String>) =
-        DictTopicOwnerEntityGeneratedRepository.findById(id.primaryKey).map { it.toImmutable() }
+    suspend fun findById(@RequestBody id: PrimaryKeyWrapper<String>) =
+        DictTopicOwnerEntityGeneratedRepository.findById(id.primaryKey)?.toImmutable()
+
+    @PutMapping("/findByIdNoWrapper", produces = [MediaType.APPLICATION_JSON_VALUE])
+    suspend fun findByIdNoWrapper(@RequestParam id: String): DictTopicOwnerEntityImmutable? {
+        val findById = DictTopicOwnerEntityGeneratedRepository.findById(id)
+        val toImmutable = findById?.toImmutable()
+        return toImmutable
+    }
 
 
     @Operation(summary = "Найти все.", tags = ["Генерированное API. Владелец топика(DictTopicOwnerEntity)"])
     @GetMapping("/findAll", produces = [MediaType.APPLICATION_JSON_VALUE])
-    fun findAll() = DictTopicOwnerEntityGeneratedRepository.findAll().map { it.toImmutable() }
+    suspend fun findAll(): Flow<DictTopicOwnerEntityImmutable> {
+        val findAll = DictTopicOwnerEntityGeneratedRepository.findAll()
+        val map = findAll.map { it.toImmutable() }
+        return map
+    }
 
     @Operation(summary = "Сохранить.", tags = ["Генерированное API. Владелец топика(DictTopicOwnerEntity)"])
     @PutMapping("/save", produces = [MediaType.APPLICATION_JSON_VALUE])
     @Transactional(propagation = Propagation.REQUIRES_NEW)
-    fun save(@RequestBody data: DictTopicOwnerEntityImmutable) =
-        DictTopicOwnerEntityGeneratedRepository.save(data.toMutable()).map { it.toImmutable() }
+    suspend fun save(@RequestBody data: DictTopicOwnerEntityImmutable) =
+        DictTopicOwnerEntityGeneratedRepository.save(data.toMutable()).toImmutable()
 
     @Operation(
         summary = "Удалить по идентификатору.",
@@ -45,12 +61,8 @@ class DictTopicOwnerEntityGeneratedRestApi(
     )
     @DeleteMapping("/deleteById", produces = [MediaType.APPLICATION_JSON_VALUE])
     @Transactional(propagation = Propagation.REQUIRES_NEW)
-    fun deleteById(@RequestBody id: PrimaryKeyWrapper<String>) =
+    suspend fun deleteById(@RequestBody id: PrimaryKeyWrapper<String>) =
         DictTopicOwnerEntityGeneratedRepository.deleteById(id.primaryKey)
-
-//    @Operation(summary = "Выбобрка с фильтром.", tags = ["Генерированное API. Владелец топика(DictTopicOwnerEntity)"])
-//    @PutMapping("/findByFilterOrIsNull", produces = [MediaType.APPLICATION_JSON_VALUE])
-//    fun findByFilterOrIsNull(filter: DictTopicOwnerEntityFilter): List<DictTopicOwnerEntityImmutable> = orIsNullRepository.findByFilterOrIsNull(filter).map { it.toImmutable() }
 
     @Operation(
         summary = "Редактировать значение.",
@@ -58,19 +70,21 @@ class DictTopicOwnerEntityGeneratedRestApi(
     )
     @PutMapping("/editEntity", produces = [MediaType.APPLICATION_JSON_VALUE])
     @Transactional(propagation = Propagation.REQUIRES_NEW)
-    fun editEntity(
+    suspend fun editEntity(
         @RequestBody editData: DictTopicOwnerEntityRestEdit
-    ) = DictTopicOwnerEntityGeneratedRepository
-        .findById(editData.primaryKey)
-        .flatMap { oldData ->
+    ): DictTopicOwnerEntityImmutable? {
+        val findById = DictTopicOwnerEntityGeneratedRepository
+            .findById(editData.primaryKey)
+        val let: DictTopicOwnerEntityImmutable? = findById?.let { oldData ->
             DictTopicOwnerEntityGeneratedRepository.save(
                 oldData.apply {
                     this@apply.id = editData.newData.id
                     this@apply.our = editData.newData.our
                     this@apply.descriptionForReport = editData.newData.descriptionForReport
-                })
-                .map { it.toImmutable() }
+                }).toImmutable()
         }
+        return let
+    }
 
 
 }
